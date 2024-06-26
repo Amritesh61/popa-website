45d9e97b1c7db84e510797777aa0537a65befc89
Amritesh61 <amritesh.raj61@gmail.com>
2024-06-26 18:54:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/FT4QsRSV_400x400.jpg" className="App-logo" alt="logo" />
+        <h1>POPA, The Solana Cat</h1>
+        <p>The Solana cat that converts degens into millionaires.</p>
+      </header>
+      <main>
+        <div className="background-banner">
+          <img src="/1500x500 (5).jpg" alt="banner" />
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
